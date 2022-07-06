2adc24b3188f661bc8081e8fd8cb63d70b935cfe
Administrator <123456@qq.com>
2022-07-06 09:16:46 +0800

    简单的新增用户完成

--- src/main/java/com/qmcz/controller/UserController.java
+++ src/main/java/com/qmcz/controller/UserController.java
@@ -4,16 +4,17 @@ import com.qmcz.base.TransformData;
 import com.qmcz.domain.User;
 import com.qmcz.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author pcf
  * @date 2022/01/27
  */
-@RestController
+@Controller
 @RequestMapping("/user")
 @CrossOrigin("*")
 public class UserController {
@@ -21,9 +22,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @ResponseBody
     @RequestMapping("/list")
     public TransformData<User> getUserList(){
         return userService.getUserList();
+    }
+
+    @RequestMapping("/to_add_page")
+    public String toAddPage(ModelMap modelMap){
+        modelMap.addAttribute("test","one");
+        return "pages/user/userAdd";
     }
 
     @RequestMapping("/add_user")
