9a66f36819f991d428d07d72b5b9f748a0fca651
LisaSu555 <1510684732@qq.com>
2022-07-07 23:59:05 +0800

    修改测试

--- src/main/java/com/qmcz/controller/CostController.java
+++ src/main/java/com/qmcz/controller/CostController.java
@@ -7,9 +7,6 @@ import com.qmcz.service.CostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 /**
  * 消费控制器
