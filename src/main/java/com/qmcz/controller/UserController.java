7fcf17436602d2ba502691431334d94941290e38
Administrator <123456@qq.com>
2022-07-10 15:45:23 +0800

    登录界面的小提示完成

--- src/main/java/com/qmcz/controller/UserController.java
+++ src/main/java/com/qmcz/controller/UserController.java
@@ -5,10 +5,13 @@ import com.qmcz.domain.User;
 import com.qmcz.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @author pcf
@@ -22,10 +25,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @ResponseBody
     @RequestMapping("/list")
-    public TransformData<User> getUserList(){
-        return userService.getUserList();
+    public String getUserList(ModelMap modelMap){
+        TransformData<User> userListTrans = userService.getUserList();
+        List<User> userList = userListTrans.getRows();
+        modelMap.addAttribute("list", userList);
+        return "pages/user/userList";
     }
 
     @RequestMapping("/to_add_page")
