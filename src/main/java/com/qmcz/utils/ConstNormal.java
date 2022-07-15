f2e2443a6ad5c46b99fc1dd85166cd1aacd06ab5
Administrator <123456@qq.com>
2022-07-15 11:10:37 +0800

    文件上传完成

---
+++ src/main/java/com/qmcz/utils/ConstNormal.java
@@ -0,0 +1,11 @@
+package com.qmcz.utils;
+
+/**
+ * 常数类
+ * @author pcf
+ * @date 2022/07/15
+ */
+public class ConstNormal {
+    public static String interfaceXlsxName = "用户增删改查接口文档.xlsx";
+    public static String interfaceXlsxPath = "D:\\pcf\\soft\\project\\java\\student-sys\\src\\main\\java\\com\\qmcz\\utils\\用户增删改查接口文档.xlsx";
+}
